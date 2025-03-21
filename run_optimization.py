0a89f51aec0fae0aa313fb94978d79e796fdc2d7
noahfrank3 <noahfrank3@outlookc.om>
2025-03-21 19:38:08 -0400

    Working on distributed computing

--- run_optimization.py
+++ run_optimization.py
@@ -1,60 +1,40 @@
-from ax.plot.pareto_frontier import plot_pareto_frontier
-from ax.plot.pareto_utils import compute_posterior_pareto_frontier
-from ax.service.ax_client import AxClient
-from ax.service.utils.instantiation import ObjectiveProperties
+import paramiko
+import json
 
-import plotly.io as pio
+from ax.service.ax_client import ObjectiveProperties
+from ax.service.scheduler import Scheduler
+from utils import *
 
-from objectives_constraints import *
+# Define functions (this will be deleted in place of actual functions)
+f_1 = lambda x: x**2
+f_2 = lambda x: (x - 1)**2
 
-n_trials = 10 # number of trials for Bayesian optimization
-n_points = 5 # number of points to show on the Pareto frontier
-
-ax_client = AxClient()
-
+# Set up ax client
+ax_client = AxClient2()
 ax_client.create_experiment(
-    name='SPH_optimization',
+    name='so_test',
     parameters=[
         {
-            'name': 'D_pt',
+            'name': 'x',
             'type': 'range',
-            'bounds': [25, 35],
-        },
-        {
-            'name': 's_d',
-            'type': 'range',
-            'bounds': [29.96, 33.96],
+            'bounds': [-10.0, 10.0]
         },
     ],
     objectives={
-        'F_slosh': ObjectiveProperties(minimize=True),
-        'V_baffle': ObjectiveProperties(minimize=True),
-    },
-    is_test=True
+        'f_1': ObjectiveProperties(minimize=True, threshold=5),
+        'f_2': ObjectiveProperties(minimize=True, threshold=5)
+    }
 )
 
-def evaluate(parameters):
-    D_pt = parameters['D_pt']
-    s_d = parameters['s_d']
-
-    F_slosh = get_F_slosh(D_pt, s_d)
-    V_baffle = get_V_baffle(D_pt, s_d)
-
-    return {'F_slosh': F_slosh, 'V_baffle': V_baffle}
-
-for _ in range(n_trials):
-    parameters, trial_idx = ax_client.get_next_trial()
-    ax_client.complete_trial(trial_index=trial_idx, raw_data=evaluate(parameters))
-
-objectives = ax_client.experiment.optimization_config.objective.objectives
-frontier = compute_posterior_pareto_frontier(
+runner = DistributedRunner()
+scheduler = DistributedScheduler(
     experiment=ax_client.experiment,
-    data=ax_client.experiment.fetch_data(),
-    primary_objective=objectives[1].metric,
-    secondary_objective=objectives[0].metric,
-    absolute_metrics=['F_slosh', 'V_baffle'],
-    num_points=n_points,
+    runner=runner
 )
-pareto_plot = plot_pareto_frontier(frontier, CI_level=0.95)
-fig = pareto_plot[0]
-pio.show(fig)
+scheduler.run_all_trials()
+
+def evaluate(params):
+    return {'f_1': (f_1(params['x']), 0), 'f_2': (f_2(params['x']), 0)}
+
+while True:
+    params, trial_index = ax_client.get_next_trial()
